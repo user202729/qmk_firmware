826cc8663a23e001eba5bdf60ef38d86a62f67b6
user202729 <25191436+user202729@users.noreply.github.com>
2020-06-28 20:58:53 +0700

    My gaming mode

--- keyboards/georgi/keymaps/default/keymap.c
+++ keyboards/georgi/keymaps/default/keymap.c
@@ -232,15 +232,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     // Gaming layer with Numpad, Very limited
     [GAMING] = LAYOUT_georgi(
-        KC_LSFT, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_DQUO,
-                                   KC_LALT, KC_SPC,  LT(GAMING_2, KC_ENT),       KC_DEL,  KC_ASTR, TO(STENO_LAYER)
+        TO(STENO_LAYER),  KC_ESC,  KC_W,  KC_A,    KC_S,    KC_D,          KC_6,    KC_7,    KC_UP,   KC_8,     KC_9, KC_LGUI,
+        TO(GAMING_2),     KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_V,          KC_N,    KC_LEFT, KC_DOWN, KC_RIGHT, KC_1, KC_LGUI,
+                                          KC_LALT, KC_SPC,  KC_SPC,        KC_DEL,  KC_ASTR, TO(STENO_LAYER)
     ),
 
     [GAMING_2] = LAYOUT_georgi(
-        KC_LSFT, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT,
-                                   KC_LALT, KC_SPC,  KC_ENT,        KC_DEL,  KC_ASTR, TO(STENO_LAYER)
+        TO(STENO_LAYER),  KC_Q,   KC_W,   KC_E,  KC_R,  KC_T,       KC_Y,  KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC,
+        TO(GAMING),       KC_A,   KC_S,   KC_D,  KC_F,  KC_G,       KC_H,  KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+                                          KC_1,  KC_C,  KC_V,       KC_N,  KC_M,   KC_2
     )
 };
 
