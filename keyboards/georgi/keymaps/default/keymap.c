683cf12dd29b221de504134ff5ec016fa3f34d6f
user202729 <25191436+user202729@users.noreply.github.com>
2020-08-13 23:13:55 +0700

    Gaming mode updated -- steno fallback

--- keyboards/georgi/keymaps/default/keymap.c
+++ keyboards/georgi/keymaps/default/keymap.c
@@ -230,20 +230,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         STN_PWR, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2,       STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR,
                                    STN_N1,  STN_A,   STN_O,         STN_E,   STN_U,   STN_N7
     ),
-    // Gaming layer with Numpad, Very limited
+    // Gaming layer (steno fallback)
     [GAMING] = LAYOUT_georgi(
+        TO(STENO_LAYER),  KC_Q,   KC_W,   KC_E,  KC_R,  KC_T,       KC_Y,  KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC,
+        TO(GAMING_2),     KC_A,   KC_S,   KC_D,  KC_F,  KC_G,       KC_H,  KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+                                          KC_1,  KC_C,  KC_V,       KC_N,  KC_M,   KC_2
+    ),
+	// True gaming layer
+    [GAMING_2] = LAYOUT_georgi(
         TO(STENO_LAYER),  KC_ESC,  KC_W,  KC_A,    KC_S,    KC_D,          KC_6,    KC_7,    KC_UP,   KC_8,     KC_9,       KC_LGUI,
-        TO(STENO_LAYER),  KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_V,          KC_N,    KC_LEFT, KC_DOWN, KC_RIGHT, LGUI(KC_1), LGUI(KC_9),
+        TO(GAMING),       KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_V,          KC_N,    KC_LEFT, KC_DOWN, KC_RIGHT, LGUI(KC_1), LGUI(KC_9),
                                           KC_LALT, KC_SPC,  KC_SPC,        KC_DEL,  KC_ASTR, TO(STENO_LAYER)
     )
-
-	/*
-    [GAMING_2] = LAYOUT_georgi(
-        TO(STENO_LAYER),  KC_Q,   KC_W,   KC_E,  KC_R,  KC_T,       KC_Y,  KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC,
-        TO(GAMING),       KC_A,   KC_S,   KC_D,  KC_F,  KC_G,       KC_H,  KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-                                          KC_1,  KC_C,  KC_V,       KC_N,  KC_M,   KC_2
-    )
-	*/
 };
 
 // Don't fuck with this, thanks.
