ff28ea119c5c05b0ade0ecfe49602febc1bedd71
user202729 <25191436+user202729@users.noreply.github.com>
2022-11-25 15:30:33 +0700

    remove STENO_LAYER entirely (have some issues though, cannot flash to board for now, hopefully the flash memory is not broken)

--- keyboards/georgi/keymaps/default/keymap.c
+++ keyboards/georgi/keymaps/default/keymap.c
@@ -219,24 +219,12 @@ uint32_t processQwerty(bool lookup) {
 // Note: If using NO_ACTION_TAPPING, LT will not work!
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    // Gaming layer (steno fallback)
+    // steno fallback
     [GAMING] = LAYOUT_georgi(
-        TO(STENO_LAYER),  KC_Q,   KC_W,   KC_E,  KC_R,  KC_T,       KC_Y,  KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC,
+        KC_NO,            KC_Q,   KC_W,   KC_E,  KC_R,  KC_T,       KC_Y,  KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC,
         KC_NO,            KC_A,   KC_S,   KC_D,  KC_F,  KC_G,       KC_H,  KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
                                           KC_1,  KC_C,  KC_V,       KC_N,  KC_M,   KC_2
     ),
-    // Steno layer?
-    [STENO_LAYER] = LAYOUT_georgi(
-        STN_FN,  STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1,       STN_ST3, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR,
-        STN_PWR, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2,       STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR,
-                                   STN_N1,  STN_A,   STN_O,         STN_E,   STN_U,   STN_N7
-    ),
-	// True gaming layer
-    [GAMING_2] = LAYOUT_georgi(
-        TO(STENO_LAYER),  KC_ESC,  KC_W,  KC_A,    KC_S,    KC_D,          KC_6,    KC_7,    KC_UP,   KC_8,     KC_9,       KC_LGUI,
-        TO(GAMING),       KC_LSFT, KC_Z,  KC_X,    KC_C,    KC_V,          KC_N,    KC_LEFT, KC_DOWN, KC_RIGHT, LGUI(KC_1), LGUI(KC_9),
-                                          KC_LALT, KC_SPC,  KC_SPC,        KC_DEL,  KC_ASTR, TO(STENO_LAYER)
-    )
 };
 
 // Don't fuck with this, thanks.
